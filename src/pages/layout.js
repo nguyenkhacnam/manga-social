418b5211528420a4220c80f4b139602425b4203d
toantran <boynhangheo2002hd@gmail.com>
2023-08-15 16:19:02 +0700

    home

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,34 +1,27 @@
 import { useState } from "react";
-<<<<<<< HEAD
-import { Link, Outlet } from "react-router-dom";
-=======
 import { Link, Outlet, json } from "react-router-dom";
->>>>>>> 78be6bdc930dc767c0e13d43a12bfdd3acd183d1
 export default function Layout({ home }) {
     const [isHovered, setIsHovered] = useState(false);
     const [isServerHovered, setIsServerHovered] = useState(false);
 
-<<<<<<< HEAD
-=======
     //handle search
-    const [input, setInput] = useState("")
-    console.log(input)
+    const [input, setInput] = useState("");
+    console.log(input);
     const fetchData = (value) => {
-        fetch('http://14.225.7.221:7979/')
+        fetch("http://14.225.7.221:7979/")
             .then((response) => response.json())
-            .then(res => {
-                console.log(res[1].data)
+            .then((res) => {
+                console.log(res[1].data);
                 // const results = data.filter((data) => {
                 //     return data && data.title_manga && data.title_manga.toLowerCase().includes(value)
                 // })
                 // console.log(results)
-            })
-    }
+            });
+    };
     const handleChange = (value) => {
-        setInput(value)
-        fetchData()
-    }
->>>>>>> 78be6bdc930dc767c0e13d43a12bfdd3acd183d1
+        setInput(value);
+        fetchData();
+    };
     const handleMouseEnter = () => {
         setIsHovered(true);
     };
@@ -56,26 +49,6 @@ export default function Layout({ home }) {
                     </div>
                 </Link>
                 <div className="menu-header">
-<<<<<<< HEAD
-                    <div
-                        className="comic"
-                        onMouseEnter={handleMouseEnter}
-                        onMouseLeave={handleMouseLeave}
-                    >
-                        <p>Comic</p>
-                        <img
-                            className="arrow-img"
-                            src={
-                                isHovered
-                                    ? "/images/Polygon cam.svg"
-                                    : "/images/Polygon 1.svg"
-                            }
-                            alt="Arrow"
-                        />
-                    </div>
-
-                    <p>Genres</p>
-=======
                     <Link to="/">
                         <div
                             className="comic"
@@ -95,8 +68,9 @@ export default function Layout({ home }) {
                         </div>
                     </Link>
 
-                    <Link to="/genres"><p>Genres</p></Link>
->>>>>>> 78be6bdc930dc767c0e13d43a12bfdd3acd183d1
+                    <Link to="/genres">
+                        <p>Genres</p>
+                    </Link>
 
                     <p>Popular</p>
 
@@ -116,27 +90,23 @@ export default function Layout({ home }) {
                             alt="Arrow"
                         />
                     </div>
-<<<<<<< HEAD
-                    <p className="contact">Contact us</p>
+                    <Link to="/contact-us">
+                        <p className="contact">Contact us</p>
+                    </Link>
                 </div>
                 <div className="avatar_search">
                     <img src="/images/search.svg "></img>
-                    <input type="text" placeholder="Search"></input>
-                    <div className="avatar">
-                        <img src="/images/usersquare.svg"></img>
-                    </div>
-=======
-                    <Link to="/contact-us"><p className="contact">Contact us</p></Link>
-                </div>
-                <div className="avatar_search">
-                    <img src="/images/search.svg "></img>
-                    <input type="text" placeholder="Search..." value={input} onChange={(e) => handleChange(e.target.value)}></input>
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        value={input}
+                        onChange={(e) => handleChange(e.target.value)}
+                    ></input>
                     <Link to="/user-profile">
                         <div className="avatar">
                             <img src="/images/usersquare.svg"></img>
                         </div>
                     </Link>
->>>>>>> 78be6bdc930dc767c0e13d43a12bfdd3acd183d1
                 </div>
             </div>
             <Outlet></Outlet>
