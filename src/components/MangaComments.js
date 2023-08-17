72fa768883dfeca0dd20b622b459e48870f60477
nguyenkhacnam <nguyenkhacnam06052001@gmail.com>
2023-08-17 15:24:56 +0700

    reponsive comments chapter

--- src/components/MangaComments.js
+++ src/components/MangaComments.js
@@ -69,66 +69,68 @@ function MangaComments() {
   //     fetchUserImages();
   // }, [comments]);
   return (
-    <div className="flex justify-center">
-      <div className="w-[1269px] bg-[#2D2D2D]">
-        <div className="flex justify-around mt-12 ml-12 mr-12">
-          <img src={image} alt="" className="w-14 h-14 rounded-full" />
-          <input
-            type="text"
-            placeholder="Give a comment"
-            className="w-[937px] h-[52px] rounded-[32px] bg-[#4B4B4B] text-2xl font-semibold pl-3 text-[#fff] outline-none"
-          />
-          <img
-            src="/images/MangaComments/Group 48096940.png"
-            alt=""
-            className="w-12 h-12"
-          />
-        </div>
-        <div className="mx-[179px]">
-          {comments.map((comment, index) => {
-            console.log(comment.user_id);
-            // const usesignle = handeleFetchUser(id)
-            return (
-              <div key={index} className="flex justify-around my-12">
-                <img src={image} alt="" className="w-14 h-14 rounded-full" />
-                <div className="flex flex-col">
-                  <div className="w-[843px] bg-[#4B4B4B] text-white">
-                    <div className="mx-12 my-6">
-                      <h2 className="block mb-[10px] text-2xl font-semibold text-white">
-                        User Name
-                      </h2>
-                      <p className="font-light">{comment.content}</p>
+    <div className="bg-black">
+        <div className="flex justify-center py-[22px] md:py-20">
+          <div className="md:w-[1269px] md:bg-[#2D2D2D] rounded-xl">
+            <div className="ml-[25px] mr-[30px] flex justify-around md:mt-12 md:ml-12 md:mr-12 ">
+              <img src={image} alt="" className="w-7 h-7 md:w-14 md:h-14 rounded-full" />
+              <input
+                type="text"
+                placeholder="Give a comment"
+                className="w-[264px] md:w-[937px] h-7 md:h-[52px] rounded-xl md:rounded-[32px] bg-[#4B4B4B] text-xs md:text-2xl md:font-semibold pl-2 md:pl-3 text-[#fff] outline-none"
+              />
+              <img
+                src="/images/MangaComments/Group 48096940.png"
+                alt=""
+                className="w-5 h-5 md:w-12 md:h-12"
+              />
+            </div>
+            <div className="mt-4 md:ml-[25px] md:mr-12 md:mx-[179px] ">
+              {comments.map((comment, index) => {
+                console.log(comment.user_id);
+                // const usesignle = handeleFetchUser(id)
+                return (
+                  <div key={index} className="flex md:justify-around md:my-12 gap-5 mx-auto mb-3">
+                    <img src={image} alt="" className="w-6 h-6 md:w-14 md:h-14 rounded-full" />
+                    <div className="flex flex-col">
+                      <div className="w-[243px] md:w-[843px] bg-[#4B4B4B] text-white rounded-xl">
+                        <div className="mx-3 my-3 md:mx-12 md:my-6">
+                          <h2 className="block mb-[10px] text-xs md:text-2xl font-semibold text-white">
+                            User Name
+                          </h2>
+                          <p className="font-light text-xs">{comment.content}</p>
+                        </div>
+                      </div>
+                      <div className="md:ml-5 flex justify-between md:mt-1 md:text-[22px] text-xs text-white">
+                        <div className="flex">
+                          <p>12/07/2023</p>
+                          <p className="block mx-1 md:mx-5">Like</p>
+                          <p className="block mx-1 md:mx-5">Reply</p>
+                          <img
+                            src="/images/MangaComments/more-horizontal.png"
+                            alt=""
+                            className="md:w-6 md:h-6 self-center"
+                          />
+                        </div>
+                        <div className="flex">
+                          <p className="block mr-2">1</p>
+                          <img
+                            src="/images/MangaComments/Frame 48096936.png"
+                            alt=""
+                            className="w-3 h-3 md:w-8 md:h-8 self-center"
+                          />
+                        </div>
+                      </div>
                     </div>
                   </div>
-                  <div className="ml-5 flex justify-between mt-1 text-[22px] text-white">
-                    <div className="flex">
-                      <p>12/07/2023</p>
-                      <p className="block mx-5">Like</p>
-                      <p className="block mx-5">Reply</p>
-                      <img
-                        src="/images/MangaComments/more-horizontal.png"
-                        alt=""
-                        className="w-6 h-6 self-center"
-                      />
-                    </div>
-                    <div className="flex">
-                      <p className="block mr-2">1</p>
-                      <img
-                        src="/images/MangaComments/Frame 48096936.png"
-                        alt=""
-                        className="w-8 h-8 self-center"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+                );
+              })}
+            </div>
+            <div className="flex justify-center my-12 text-[32px] text-white font-semibold cursor-pointer">
+              <p>See More Comment</p>
+            </div>
+          </div>
         </div>
-        <div className="flex justify-center my-12 text-[32px] text-white font-semibold cursor-pointer">
-          <p>See More Comment</p>
-        </div>
-      </div>
     </div>
   );
 }
